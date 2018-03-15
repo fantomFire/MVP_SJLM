a77bb02640c26f4e409af623ef0142028482bb09
fantomFire <yilong8903@163.com>
2018-03-15 15:28:07 +0800

    sss

--- app/src/main/java/com/zhss/sjlm/ui/adapter/AddressAdapter.java
+++ app/src/main/java/com/zhss/sjlm/ui/adapter/AddressAdapter.java
@@ -9,23 +9,23 @@ import com.zhss.sjlm.bean.AddressALLBean;
  * Created by win7-64 on 2018/3/14.
  */
 
-public class AddressAdapter  extends BaseQuickAdapter<AddressALLBean.DataBean.AddressBean,BaseViewHolder>{
+public class AddressAdapter extends BaseQuickAdapter<AddressALLBean.DataBean.AddressBean, BaseViewHolder> {
     public AddressAdapter() {
         super(R.layout.adapter_address);
     }
 
     @Override
-    protected void convert(BaseViewHolder helper,AddressALLBean.DataBean.AddressBean item) {
-        helper.setText(R.id.txt_name,item.getTrue_name())
-                .setText(R.id.txt_mobile,item.getTel_phone())
-                .setText(R.id.txt_address,item.getArea_info()+item.getAddress())
+    protected void convert(BaseViewHolder helper, AddressALLBean.DataBean.AddressBean item) {
+        helper.setText(R.id.txt_name, item.getTrue_name())
+                .setText(R.id.txt_mobile, item.getTel_phone())
+                .setText(R.id.txt_address, item.getArea_info() + item.getAddress())
                 .addOnClickListener(R.id.ll_modify_address)
                 .addOnClickListener(R.id.ll_dele)
                 .addOnClickListener(R.id.ll_defualt);
         String is_default = item.getIs_default();
-        if (is_default.equals("0")){
-           helper.getView(R.id.iv_address).setBackgroundResource(R.mipmap.szl);
-        }else {
+        if (is_default.equals("0")) {
+            helper.getView(R.id.iv_address).setBackgroundResource(R.mipmap.szl);
+        } else {
             helper.getView(R.id.iv_address).setBackgroundResource(R.mipmap.sz1);
 
         }
